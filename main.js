1698e0439019accb902637e98e22391d0335044e
Chad Malmrose <chadmalmrose1@gmail.com>
2023-11-06 23:55:38 -0700

    More presentable

---
+++ main.js
@@ -0,0 +1,19 @@
+const mySkills = [
+    "JavaScript", "C#", "Python",
+    "C++"
+];
+
+function generateProjectCard() {
+    
+}
+function generateSkills() {
+    for (const skill of mySkills) {
+        let container = document.createElement('div');
+        container.className = 'skill';
+        let span = document.createElement('span');
+        span.innerText = skill;
+        container.appendChild(span);
+        document.getElementById('skillsContainer').appendChild(container);
+    }
+}
+generateSkills();
